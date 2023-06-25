244ac27da494629d6661c0d149ca53de95a776b7
Lyssenko Alexandr <lyssenko.alex@gmail.com>
2023-06-25 21:41:23 +0600

    init

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const ItemsCategory_controller_1 = __importDefault(require("./controllers/ItemsCategory.controller"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+app.use(express_1.default.json());
+app.use(express_1.default.static("public"));
 app.get('/', (_req, res) => {
     return res.send('Express Typescript on Vercel 5');
 });
 app.get('/ping', (_req, res) => {
     return res.send('pong 🏓');
 });
+app.use("/items_category", ItemsCategory_controller_1.default);
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import express, { Request, Response } from 'express'
+import itemsCategoryController from "./controllers/ItemsCategory.controller";
 
 const app = express()
 const port = process.env.PORT || 8080
 
+
+app.use(express.json());
+app.use(express.static("public"));
+
+
 app.get('/', (_req: Request, res: Response) => {
-	return res.send('Express Typescript on Vercel 3')
+	return res.send('Express Typescript on Vercel 5')
 })
 
 app.get('/ping', (_req: Request, res: Response) => {
 	return res.send('pong 🏓')
 })
-
+app.use("/items_category", itemsCategoryController);
 app.listen(port, () => {
 	return console.log(`Server is listening on ${port}`)
 })
