ba48d9ed864bfea6fce3c785f00c5098f3286230
Lyssenko Alexandr <lyssenko.alex@gmail.com>
2023-06-25 22:05:01 +0600

    init

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const items_controller_1 = __importDefault(require("./controllers/items.controller"));
+const suppliers_controller_1 = __importDefault(require("./controllers/suppliers.controller"));
+const users_controller_1 = __importDefault(require("./controllers/users.controller"));
+const actions_controller_1 = __importDefault(require("./controllers/actions.controller"));
+const suppliersStorages_controller_1 = __importDefault(require("./controllers/suppliersStorages.controller"));
+const storages_controller_1 = __importDefault(require("./controllers/storages.controller"));
 const ItemsCategory_controller_1 = __importDefault(require("./controllers/ItemsCategory.controller"));
+const ItemsSubcategory_controller_1 = __importDefault(require("./controllers/ItemsSubcategory.controller"));
+const location_controller_1 = __importDefault(require("./controllers/location.controller"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
 app.use(express_1.default.json());
@@ -15,7 +23,15 @@ app.get('/', (_req, res) => {
 app.get('/ping', (_req, res) => {
     return res.send('pong 🏓');
 });
+app.use("/items", items_controller_1.default);
+app.use("/suppliers", suppliers_controller_1.default);
+app.use("/users", users_controller_1.default);
+app.use("/supply", actions_controller_1.default);
+app.use("/suppliers_controllers", suppliersStorages_controller_1.default);
+app.use("/storages", storages_controller_1.default);
 app.use("/items_category", ItemsCategory_controller_1.default);
+app.use("/items_subcategory", ItemsSubcategory_controller_1.default);
+app.use("/location", location_controller_1.default);
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import express, { Request, Response } from 'express'
+import itemController from "./controllers/items.controller";
+import suppliersController from "./controllers/suppliers.controller";
+import usersController from "./controllers/users.controller";
+import actionsController from "./controllers/actions.controller";
+import suppliersStoragesController from './controllers/suppliersStorages.controller'
+import storagesController from './controllers/storages.controller'
 import itemsCategoryController from "./controllers/ItemsCategory.controller";
+import itemsSubcategoryController from "./controllers/ItemsSubcategory.controller";
+import locationController from "./controllers/location.controller";
+
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -16,7 +25,16 @@ app.get('/', (_req: Request, res: Response) => {
 app.get('/ping', (_req: Request, res: Response) => {
 	return res.send('pong 🏓')
 })
+app.use("/items", itemController);
+app.use("/suppliers", suppliersController);
+app.use("/users", usersController);
+app.use("/supply", actionsController);
+app.use("/suppliers_controllers", suppliersStoragesController);
+app.use("/storages", storagesController);
 app.use("/items_category", itemsCategoryController);
+app.use("/items_subcategory", itemsSubcategoryController);
+app.use("/location", locationController);
+
 app.listen(port, () => {
 	return console.log(`Server is listening on ${port}`)
 })
